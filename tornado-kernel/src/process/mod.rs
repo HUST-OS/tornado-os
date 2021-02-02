8f8a9825310f7b1519afbb2e98c1cb6c7487cebb
luojia65 <me@luojia.cc>
2021-02-02 17:03:35 +0800

    Modify: link section of shared data & text

--- tornado-kernel/src/process/mod.rs
+++ tornado-kernel/src/process/mod.rs
@@ -9,18 +9,14 @@ pub use process::{Process, ProcessInner};
 
 use crate::algorithm::FifoScheduler;
 
-/// 所有用户任务的调度器
-#[link_section = ".shared_rw.data"]
+/// 所有任务的调度器
+#[link_section = ".shared_data"]
 pub static SHARED_SCHEDULER: spin::Mutex<FifoScheduler<Task>> = 
     spin::Mutex::new(FifoScheduler::new());
 
 /// 共享的包含Future在用户空间的地址
 pub struct SharedTaskHandle(pub usize);
 
-#[link_section = ".shared_x.text"]
+#[link_section = ".shared_text"]
 #[allow(unused)]
 pub fn shared_add_task(handle: SharedTaskHandle) {}
-
-/// 所有内核任务的调度器
-pub static LOCAL_SCHEDULER: spin::Mutex<FifoScheduler<Task>> = 
-    spin::Mutex::new(FifoScheduler::new());
