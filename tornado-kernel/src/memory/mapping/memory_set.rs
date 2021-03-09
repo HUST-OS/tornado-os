8e9080dc6a5c599a0206a4582e48982e7f74a343
luojia65 <me@luojia.cc>
2021-03-09 18:06:45 +0800

    Small fix

--- tornado-kernel/src/memory/mapping/memory_set.rs
+++ tornado-kernel/src/memory/mapping/memory_set.rs
@@ -78,7 +78,7 @@ impl MemorySet {
             Segment {
                 map_type: MapType::Linear,
                 range: VirtualAddress(_sshared_text as usize)..VirtualAddress(_eshared_text as usize),
-                flags: Flags::EXECUTABLE
+                flags: Flags::EXECUTABLE // 没有READABLE
             },
             // 剩余内存空间，rw-
             Segment {
