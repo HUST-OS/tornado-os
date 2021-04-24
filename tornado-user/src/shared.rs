7b305f171b483bb5a26a7a65a2c8d618eca3d7a0
luojia65 <me@luojia.cc>
2021-04-24 23:14:54 +0800

    Bug fix

--- tornado-user/src/shared.rs
+++ tornado-user/src/shared.rs
@@ -35,7 +35,7 @@ pub extern "C" fn user_should_switch(_handle: &SharedTaskHandle) -> bool {
 }
 
 // 该执行器目前是测试使用，当轮询到一个完成的任务就退出了
-pub fn run_until_idle(
+pub fn run_until_ready(
     peek_task: impl Fn() -> TaskResult,
     delete_task: impl Fn(usize) -> bool,
 ) {
