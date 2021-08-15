bb6487eb704ae8a28eb8e0d846ccd6f66db4bc30
Chunchi Che <56007649+SKTT1Ryze@users.noreply.github.com>
2021-08-15 17:14:52 +0800

    finish switch analysis

--- tornado-kernel/src/main.rs
+++ tornado-kernel/src/main.rs
@@ -209,19 +209,19 @@ pub extern "C" fn rust_main(hart_id: usize) -> ! {
 
     // 通过一些任务从文件系统中加载用户的二进制文件和准备用户的上下文
     let task_6 = task::new_kernel(
-        user::prepare_user("analysis0.bin", stack_handle.end.0),
+        user::prepare_user("yield-task0.bin", stack_handle.end.0),
         process.clone(),
         shared_payload.shared_scheduler,
         shared_payload.shared_set_task_state,
     );
     let task_7 = task::new_kernel(
-        user::prepare_user("analysis1.bin", stack_handle.end.0),
+        user::prepare_user("yield-task1.bin", stack_handle.end.0),
         process.clone(),
         shared_payload.shared_scheduler,
         shared_payload.shared_set_task_state,
     );
     let task_8 = task::new_kernel(
-        user::prepare_user("analysis.bin", stack_handle.end.0),
+        user::prepare_user("async-read.bin", stack_handle.end.0),
         process.clone(),
         shared_payload.shared_scheduler,
         shared_payload.shared_set_task_state,
@@ -232,12 +232,32 @@ pub extern "C" fn rust_main(hart_id: usize) -> ! {
         shared_payload.shared_scheduler,
         shared_payload.shared_set_task_state,
     );
-
+    let task_10 = task::new_kernel(
+        user::prepare_user("analysis0.bin", stack_handle.end.0),
+        process.clone(),
+        shared_payload.shared_scheduler,
+        shared_payload.shared_set_task_state,
+    );
+    let task_11 = task::new_kernel(
+        user::prepare_user("analysis1.bin", stack_handle.end.0),
+        process.clone(),
+        shared_payload.shared_scheduler,
+        shared_payload.shared_set_task_state,
+    );
+    let task_12 = task::new_kernel(
+        user::prepare_user("analysis2.bin", stack_handle.end.0),
+        process.clone(),
+        shared_payload.shared_scheduler,
+        shared_payload.shared_set_task_state,
+    );
     unsafe {
-        // shared_payload.add_task(hart_id, address_space_id, task_6.task_repr());
+        shared_payload.add_task(hart_id, address_space_id, task_6.task_repr());
         shared_payload.add_task(hart_id, address_space_id, task_7.task_repr()); 
-        shared_payload.add_task(hart_id, address_space_id, task_8.task_repr());
+        // shared_payload.add_task(hart_id, address_space_id, task_8.task_repr());
         // shared_payload.add_task(hart_id, address_space_id, task_9.task_repr());
+        // shared_payload.add_task(hart_id, address_space_id, task_10.task_repr());
+        // shared_payload.add_task(hart_id, address_space_id, task_11.task_repr());
+        // shared_payload.add_task(hart_id, address_space_id, task_12.task_repr());
     }
 
     // 运行执行器
--- tornado-user/src/bin/analysis0.rs
+++ tornado-user/src/bin/analysis0.rs
@@ -11,7 +11,7 @@ extern crate tornado_user;
 use tornado_user::{execute_async, spawn, reset_timer, read_timer};
 
 async fn a(_x: usize) {
-    // println!("[analysis] task: {}", x);
+    // println!("[analysis] task: {}", _x);
 }
 
 // 异步main函数，由entry调用execute_async_main
@@ -20,7 +20,8 @@ fn main() -> i32 {
     for i in 0..100 {
         spawn(a(i));
     }
-    println!("[analysis] timer: {}", read_timer());
+    reset_timer();
     execute_async();
+    println!("[analysis] timer: {}", read_timer());
     0
 }
