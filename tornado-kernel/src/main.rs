3e6622b1f86bf666ee93b86995aae911381857b2
Luo Jia <me@luojia.cc>
2021-08-17 17:43:47 +0800

    小修改

--- tornado-kernel/src/main.rs
+++ tornado-kernel/src/main.rs
@@ -276,8 +276,8 @@ pub extern "C" fn rust_main(hart_id: usize) -> ! {
     );
     unsafe {
         // 任务切换演示
-        // shared_payload.add_task(hart_id, address_space_id, task_6.task_repr());
-        // shared_payload.add_task(hart_id, address_space_id, task_7.task_repr());
+        shared_payload.add_task(hart_id, address_space_id, task_6.task_repr());
+        shared_payload.add_task(hart_id, address_space_id, task_7.task_repr());
 
         // 异步IO系统调用演示
         // shared_payload.add_task(hart_id, address_space_id, task_8.task_repr());
@@ -294,7 +294,7 @@ pub extern "C" fn rust_main(hart_id: usize) -> ! {
         // shared_payload.add_task(hart_id, address_space_id, task_13.task_repr());
 
         // 数据库功能实验
-        shared_payload.add_task(hart_id, address_space_id, task_14.task_repr());
+        // shared_payload.add_task(hart_id, address_space_id, task_14.task_repr());
     }
 
     // 运行执行器
